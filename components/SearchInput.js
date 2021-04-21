90e78bcdb49357f703b88bc2d6ee468a7691a4c7
Gaurav Yadav <gaurav@Gauravs-MacBook-Air.local>
2021-04-22 02:26:57 +0530

    fixed bug

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, TextInput, View } from 'react-native'
+import PropTypes from 'prop-types'
 
 export default class SearchInput extends React.Component {
 
